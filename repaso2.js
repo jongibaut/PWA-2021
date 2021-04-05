2eb188d596e674b2cccfd23e661d423a90749684
jongibaut <jongibaut@gmail.com>
2021-04-05 11:09:39 -0300

    clase 2

--- repaso2.js
+++ repaso2.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const URL = 'https://rickandmortyapi.com/api/character';
 const get = (url) => {
     const data = axios.get(url).then(res => console.log(res.data.results)).catch(e => console.log(e));
     //return data;
@@ -17,17 +17,17 @@ const getAsync = async (url) => {
 }
 const getPersonajes = async() => {
     try{
-    const personajes = await getAsync('https://rickandmortyapi.com/api/character');
+    const personajes = await getAsync(URL);
     console.log(personajes);
     }
     catch(error){
         console.log(error);
     }
 }
-//map, filter, find
+//map, filter
 const getPersonajesNombre = async() => {
     try{
-    const personajes = await getAsync('https://rickandmortyapi.com/api/character');
+    const personajes = await getAsync(URL);
     /*personajes.data.results.forEach(personaje => {
         nombreP.push(personaje.name);
     });*/
@@ -38,6 +38,32 @@ const getPersonajesNombre = async() => {
         console.log(error);
     }
 }
+const getGenero = async (gender = "Female") => {
+    try {
+    const info = await getAsync(URL);
+    const personajes = info.data.results;
+    const filtrado = personajes.filter(personaje => personaje.gender === gender);
+    console.log(filtrado);
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+const getSingle = async(id) => {
+    try{
+    const info = await getAsync(`${URL}/${id}`); // URL + '/' + id
+    const personaje = info.data;
+    //console.log(personaje);
+    const {species, name, status} = personaje;
+    console.log(name, species, status);
+    }
+    catch(e){
+        console.error(e);
+    }
+
+}
 //cuando quiero consumir en otra parte del programa un dato de una funcion async await, lo tengo que consumir dentro de otra funcion async await.
 getPersonajes();
 getPersonajesNombre();
+getGenero();
+getSingle(1);
