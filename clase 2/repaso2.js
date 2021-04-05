4b1eb269f3db799238842e338d91a916577aae0c
jongibaut <jongibaut@gmail.com>
2021-04-05 11:51:28 -0300

    clase 2 completa

--- clase 2/repaso2.js
+++ clase 2/repaso2.js
@@ -53,7 +53,7 @@ const getSingle = async(id) => {
     try{
     const info = await getAsync(`${URL}/${id}`); // URL + '/' + id
     const personaje = info.data;
-    //console.log(personaje);
+    //console.log(personaje); 
     const {species, name, status} = personaje;
     console.log(name, species, status);
     }
@@ -62,8 +62,62 @@ const getSingle = async(id) => {
     }
 
 }
+/* la API de rick y morty:
+Hacer un array con todos los personajes con status: "Alive" y species: "Human".
+Hacer otro array con todos los personajes con species: "Alien".
+
+En la pokeAPI:
+Conectarse a la pokeAPI, y hacer un get de todos los pokemons y hacer un getSingle.*/
+const getVivos = async(status = "Alive", species = "Human") => {
+    try{
+    const info = await getAsync(URL);
+    const personajes = info.data.results;
+    const vivos = personajes.filter(personaje => personaje.status === status && personaje.species === species);
+    console.log(vivos);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+const getAlien = async(species = "Alien") => {
+    try{
+    const info = await getAsync(URL);
+    const personajes = info.data.results;
+    const aliens = personajes.filter(personaje => personaje.species === species);
+    console.log(aliens);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+
+
+const getPokemon = async() => {
+    try {
+    const info = await getAsync('https://pokeapi.co/api/v2/pokemon');
+    const pokemons = info.data.results;
+    console.log(pokemons);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+const getPokemonSingle = async(id) => {
+    try {
+    const info = await getAsync(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemon = info.data;
+    console.log(pokemon);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
 //cuando quiero consumir en otra parte del programa un dato de una funcion async await, lo tengo que consumir dentro de otra funcion async await.
 getPersonajes();
 getPersonajesNombre();
 getGenero();
 getSingle(1);
+getVivos();
+getAlien();
+getPokemon();
+getPokemonSingle(1);
